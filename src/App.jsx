71aac63806cedd0a0e13da5a40640a9ea04eac7f
Leonardo Stortoni <leodaniel1978@gmail.com>
2022-08-04 23:03:01 -0300

    Cambios en el App

--- src/App.jsx
+++ src/App.jsx
@@ -15,46 +15,15 @@ import {
 import Contacto from "./components/Contacto";
 import Nosotros from "./components/Nosotros";
 import Inicio from "./components/Inicio";
+import ComponenteConRouter from "./components/ComponenteConRouter";
 
 function App() {
-  const navLinkStyles = ({ isActive }) => {
-    return {
-      fontWeight: isActive ? "bold" : "normal",
-      textDecoration: isActive ? "none" : "underline"
-    };
-  };
 
 
   return (
-    <Router>
-      <div className="container mt-5">
-        <h1>Proyecto componentes, props , router</h1>
-        {/**
-       <ComponenteComentariosYProps/>
-        */ }
-        <div className="container">
-            <NavLink className="btn btn-dark" to="/" style={navLinkStyles} >
-                Inicio
-            </NavLink>
-            <NavLink className="btn btn-dark" to="/contacto" style={navLinkStyles} >
-                Contacto
-            </NavLink>
-            <NavLink className="btn btn-dark" to="/nosotros"  style={navLinkStyles} >
-                Nosotros
-            </NavLink>
-        </div>
-      <Routes>
-       
-        <Route path="/" element={<Inicio/>} />
-         
-       
-        <Route path="/contacto" element={<Contacto/>} />
 
-        <Route path="/nosotros" element={<Nosotros/>} />
-
-      </Routes>
-      </div>
-    </Router>
+     
+     <ComponenteConRouter/>
   );
 }
 
