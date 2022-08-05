a6e45449b1bcdd5bb5e3e60340cc8390cb21ec64
Lstortoni <leodaniel1978@gmail.com>
2022-08-05 14:13:45 -0300

    Se agregan funcionalidad con ruteo dinamico y nuevos componentes

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,10 +1,17 @@
 import React,{useEffect} from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
 
 const Nosotros = () => {
 
   //const datos=[{id:1, nombre:'leo'  },{id:2, nombre:'santi'  }, {id:3, nombre:'salva'  }] 
 
-  const [equipo,setEquipo] = React.useState(null)
+  const [equipo,setEquipo] = React.useState([])
   //llamamos a una funcion de flecha para usar useEffect
  useEffect  (()=>{
     //  console.log('Use Efect : se pinta')     
@@ -13,18 +20,35 @@ const Nosotros = () => {
  },[]);
 
  const obtenerDatos= async()=>{
-    const data = await fetch ('https://jsonplaceholder.typicode.com/users')
+    const data = await fetch ('https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations')
     const users=await data.json()
     //console.log(users)
-    setEquipo(users)
+    setEquipo(users.civilizations)
  }
 
   return (
     <div>
-        <h1>
-        Nosotros
-        </h1>
-        
+        <h1> Nosotros </h1>
+        <ul  className='list-group'>
+           {
+             
+             
+             equipo.map( (item,index)=> (
+
+                 
+              <li key={item.id}> 
+              <Link to={`/nosotros/${item.id}`}>
+                     {item.name} - {item.expansion}
+               
+               </Link>
+                </li>
+               )
+             
+            )
+                   
+          }
+        </ul>
+
          
     </div>
   )
---
+++ src/components/User.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const User = () => {
+  
+   console.log(useParams());
+  
+    return (
+    <div>
+        User
+    </div>
+  )
+}
+
+export default User
