5b91d86eeed58c7b088a14277ad1f002c4a918a5
Leonardo Stortoni <leodaniel1978@gmail.com>
2022-08-09 10:20:41 -0300

    Se sube

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -3,11 +3,29 @@ import { useParams } from 'react-router-dom'
 
 const User = () => {
   
+  const {id} = useParams()   // es muy importante el nombre, aca entre llaves pongo id, ya que donde defini el route le puse ese nombre, pero si le ponia otro nombtre aca habia que ponerlo.
+
+  const [pueblo,setPueblo] = React.useState([])
+  //llamamos a una funcion de flecha para usar useEffect
+ React.useEffect  (()=>{
+    //  console.log('Use Efect : se pinta')     
+    //  setEquipo(datos)
+    obtenerDatos()
+ },[]);
+
+ const obtenerDatos= async()=>{
+    const data = await fetch (`https://age-of-empires-2-api.herokuapp.com/api/v1/civilization/${id}`)    //las llaves invertidas van porque se accede al ide con llaves
+    const datopueblo=await data.json()
+    //console.log(users)
+    setPueblo(datopueblo)
+ }
    console.log(useParams());
+   
   
     return (
     <div>
-        User
+        <h3>{pueblo.name}</h3>
+        <p>{pueblo.team_bonus}</p>
     </div>
   )
 }
